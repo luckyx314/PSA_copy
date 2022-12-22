f1e03c825394bc49e9cc43d68b46169c17a9865e
Lucky Jones Uayan <luckyjonesbuayan@gmail.com>
2022-12-23 01:22:23 +0800

    sidebar navlinks will be highlighted according to section

--- code/index.js
+++ code/index.js
@@ -1,26 +1,53 @@
 const userIcon = () => {
-  const i = document.createElement("i");
-  i.setAttribute("class", "fa-solid fa-user icon");
-  return i;
-}
+    const i = document.createElement("i");
+    i.setAttribute("class", "fa-solid fa-user icon");
+    return i;
+};
 
 const generatePopulationIcons = () => {
-  const popIcons = document.getElementById("population-icons");
-  let ctr = 0;
+    const popIcons = document.getElementById("population-icons");
+    let ctr = 0;
 
-  while(ctr < 42) {
-    const person = userIcon();
-    popIcons.appendChild(person);
+    while (ctr < 42) {
+        const person = userIcon();
+        popIcons.appendChild(person);
 
-    if (ctr > 21) {
-      person.classList.add("female");
-    } else {
-      person.classList.add("male");
+        if (ctr > 21) {
+            person.classList.add("female");
+        } else {
+            person.classList.add("male");
+        }
+        ctr++;
     }
-    ctr++;
-  }
-}
+};
 
+const highlightSidebarNav = () => {
+    const sections = document.querySelectorAll("section");
+    const sidebarLi = document.querySelectorAll("nav#sidebar ul li");
+    window.addEventListener("scroll", () => {
+        let current = "";
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if (scrollY > sectionTop - sectionHeight / 3) {
+                current = section.getAttribute("id");
+            }
+        });
+        sidebarLi.forEach((li) => {
+            li.childNodes[1].childNodes[1].classList.remove("active");
+            li.childNodes[1].childNodes[3].classList.remove("active");
+
+            if (li.classList.contains(current)) {
+                li.childNodes[1].childNodes[1].classList.add("active");
+                li.childNodes[1].childNodes[3].classList.add("active");
+            }
+        });
+    });
+};
 
 // population section
 generatePopulationIcons();
+
+// sidebar nav
+highlightSidebarNav();
