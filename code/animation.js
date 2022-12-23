77b1e46564d230a98a09db4e893a2dfce5d2af68
Lucky Jones Uayan <luckyjonesbuayan@gmail.com>
2022-12-23 11:27:16 +0800

    add contact page

--- code/animation.js
+++ code/animation.js
@@ -11,6 +11,25 @@ const setObserver = (className) => {
     return observer;
 };
 
+const realTimeClock = () => {
+    let rtClock = new Date();
+    let hrs = rtClock.getHours();
+    let min = rtClock.getMinutes();
+    let sec = rtClock.getSeconds();
+
+    let ampPm = (hrs < 12) ? "AM" : "PM";
+
+    hrs = (hrs > 12) ? hrs - 12 : hrs;
+
+    hrs = ("0" + hrs).slice(-2);
+    min = ("0" + min).slice(-2);
+    sec = ("0" + sec).slice(-2);
+
+    document.getElementById("clock").innerHTML = hrs + ":" + min + ":" + sec + " " + ampPm;
+    let t = setTimeout(realTimeClock, 500);
+
+}
+
 // observers
 const showObserver = setObserver("show");
 const fadeObserver = setObserver("fade-in");
@@ -25,3 +44,7 @@ const ungrownElements = document.querySelectorAll(".ungrown");
 // observe elements
 hiddenElements.forEach((el) => showObserver.observe(el));
 fadedElements.forEach((el) => fadeObserver.observe(el));
+
+
+// clock
+realTimeClock();
